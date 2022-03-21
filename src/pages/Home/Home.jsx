6adccf5a7700403b14ed069cd846e308825ba611
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2022-03-21 07:30:31 +0100

    corrections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import * as React from "react";
+
+// import Box from "@mui/material/Box";
+// import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+// import CardContent from "@mui/material/CardContent";
+// import CardHeader from "@mui/material/CardHeader";
+// import StarIcon from "@mui/icons-material/StarBorder";
+
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import StarIcon from "@mui/icons-material/StarBorder";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
@@ -43,75 +45,75 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
-const tiers = [
-  {
-    title: "Free",
-    price: "0",
-    description: [
-      "10 users included",
-      "2 GB of storage",
-      "Help center access",
-      "Email support",
-    ],
-    buttonText: "Sign up for free",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Pro",
-    subheader: "Most popular",
-    price: "15",
-    description: [
-      "20 users included",
-      "10 GB of storage",
-      "Help center access",
-      "Priority email support",
-    ],
-    buttonText: "Get started",
-    buttonVariant: "contained",
-  },
-  {
-    title: "Enterprise",
-    price: "30",
-    description: [
-      "50 users included",
-      "30 GB of storage",
-      "Help center access",
-      "Phone & email support",
-    ],
-    buttonText: "Contact us",
-    buttonVariant: "outlined",
-  },
-];
+// const tiers = [
+//   {
+//     title: "Free",
+//     price: "0",
+//     description: [
+//       "10 users included",
+//       "2 GB of storage",
+//       "Help center access",
+//       "Email support",
+//     ],
+//     buttonText: "Sign up for free",
+//     buttonVariant: "outlined",
+//   },
+//   {
+//     title: "Pro",
+//     subheader: "Most popular",
+//     price: "15",
+//     description: [
+//       "20 users included",
+//       "10 GB of storage",
+//       "Help center access",
+//       "Priority email support",
+//     ],
+//     buttonText: "Get started",
+//     buttonVariant: "contained",
+//   },
+//   {
+//     title: "Enterprise",
+//     price: "30",
+//     description: [
+//       "50 users included",
+//       "30 GB of storage",
+//       "Help center access",
+//       "Phone & email support",
+//     ],
+//     buttonText: "Contact us",
+//     buttonVariant: "outlined",
+//   },
+// ];
 
-const footers = [
-  {
-    title: "Company",
-    description: ["Team", "History", "Contact us", "Locations"],
-  },
-  {
-    title: "Features",
-    description: [
-      "Cool stuff",
-      "Random feature",
-      "Team feature",
-      "Developer stuff",
-      "Another one",
-    ],
-  },
-  {
-    title: "Resources",
-    description: [
-      "Resource",
-      "Resource name",
-      "Another resource",
-      "Final resource",
-    ],
-  },
-  {
-    title: "Legal",
-    description: ["Privacy policy", "Terms of use"],
-  },
-];
+// const footers = [
+//   {
+//     title: "Company",
+//     description: ["Team", "History", "Contact us", "Locations"],
+//   },
+//   {
+//     title: "Features",
+//     description: [
+//       "Cool stuff",
+//       "Random feature",
+//       "Team feature",
+//       "Developer stuff",
+//       "Another one",
+//     ],
+//   },
+//   {
+//     title: "Resources",
+//     description: [
+//       "Resource",
+//       "Resource name",
+//       "Another resource",
+//       "Final resource",
+//     ],
+//   },
+//   {
+//     title: "Legal",
+//     description: ["Privacy policy", "Terms of use"],
+//   },
+// ];
 
 function Home() {
 
